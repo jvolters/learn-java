f00760adf2745f96b3e37fe18c27a9d36491a311
randyvolters <jvolters@live.com>
2016-10-14 21:05:47 +0100

    Added from Learn Java the hard way. Changes to be committed: 	new file:   FirstProg.class 	new file:   FirstProg.java 	new file:   GasReceipt.class 	new file:   GasReceipt.java

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "I am determined to learn how to code java. " );
+		System.out.println( "Today's date is: 10/14/2016" );
+	}
+}
---
+++ GasReceipt.java
@@ -0,0 +1,16 @@
+public class GasReceipt {
+	public static void main( String[] args ){
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|      CORNER STORE      |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|  2016-10-14  03:58 PM  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|  Gallons:       10.870 |" );
+		System.out.println( "|  Price/Gal:     $2.089 |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|  Fuel Total:    $22.71 |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
