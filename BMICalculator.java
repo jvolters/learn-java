ba4f75ac5a19a3c5ec0fb53318ada45c25e92071
randyvolters <jvolters@live.com>
2016-10-17 14:15:02 +0100

    Changes to be committed: 	modified:   BMICalculator.java

--- BMICalculator.java
+++ BMICalculator.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class BMICalculator {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		double h, lb, bmi;
+		double m, kg, bmi;
 		
-		System.out.println("Your height in feet: ");
-		h = keyboard.nextDouble();
+		System.out.println("Your height in meters: ");
+		m = keyboard.nextDouble();
 		
 		System.out.println("Your weight in kilos: ");
-		lb = keyboard.nextDouble();
+		kg = keyboard.nextDouble();
 		
-		bmi= lb / (h*h);
+		bmi= kg / (m*m);
 		
 		System.out.println("Your BMI is: "+bmi);
 		
