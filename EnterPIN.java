505f7de68fc9239a176fafd33cf46ac00423a333
randyvolters <jvolters@live.com>
2016-10-17 20:26:17 +0100

    Added Lesson #25. Changes to be committed: 	new file:   EnterPIN.class 	new file:   EnterPIN.java

---
+++ EnterPIN.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EnterPIN {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		int pin, entry;
+		
+		pin = 12345;
+		
+		System.out.println("WELCOME TO JAVA BANK.");
+		System.out.println("PLEASE ENTER YOUR PIN: ");
+		entry=keyboard.nextInt();
+		
+		while(entry != pin){
+			System.out.println("SORRY. THIS IS NOT YOUR PIN.  PLEASE RE-ENTER.");
+			System.out.println("PLEASE ENTER YOUR PIN: ");
+			entry=keyboard.nextInt();
+		}
+		
+		System.out.println("\nPIN ACCEPTED. BUT, UH-OH, YOU HAVE NO MONEY IN YOUR ACCOUNT.");
+	}
+}
