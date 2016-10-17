c1a035da232e635622f8edb4c1edddfae57684cb
randyvolters <jvolters@live.com>
2016-10-17 20:07:23 +0100

    modified:   DictionaryOrder.java 	Added comment explaining the .compareTo() method.

--- DictionaryOrder.java
+++ DictionaryOrder.java
@@ -13,6 +13,18 @@ public class DictionaryOrder {
 		System.out.println( "c++ isn't a made-up language!" );
 	if ( name.compareTo("c++") > 0 )
 		System.out.println( name + " comes AFTER c++" );
+	/*
+	You compare Strings to each other using the String object’s .compareTo() method. The .compareTo()
+	method doesn’t work the way you probably expect, but there is genius in how it works.
+	The comparison involves two Strings. The first String is the one to the left of the .compareTo(). The
+	second String is the one in the parentheses. And the comparison simplifies to an integer! If we call
+	the first one self and the second one other it would look like this:
+	int n = self.compareTo(other);
+	So self compares itself to other. If self is identical to other (the same length and every character the
+	same), then n would be set to 0. If self comes before other alphabetically, then n would be set to a
+	negative number (a number less than 0). And if self comes after other alphabetically, then n would
+	be set to a positive number (a number greater than 0).
+	*/
 
 	if ( name.compareTo("go") < 0 )
 		System.out.println( name + " comes BEFORE go" );
