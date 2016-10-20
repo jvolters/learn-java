b8146e575bbd5df3bd6f39bbdd43d91914fb495b
randyvolters <jvolters@live.com>
2016-10-20 19:39:54 +0100

    InputOutput takes keyboard entries and writes to a file. Changes to be committed: 	modified:   InputOutput.class 	modified:   InputOutput.java 	modified:   LOTR.txt

--- InputOutput.java
+++ InputOutput.java
@@ -6,15 +6,28 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class InputOutput {
-		public static void main(String[] args) throws Exception {
-			Scanner kbdinput = new Scanner(System.in);
-			String input;
+	public static void main(String[] args) throws Exception {
+		Scanner kbdinput = new Scanner(System.in);
+		PrintWriter fileout = new PrintWriter("LOTR.txt");
+		fileout.println("");
+		String input;
+		System.out.println("");
+		System.out.println("Program accepts keyboard input and writes to a file.");
+		System.out.println("When finished typing a line, press Enter.");
+		System.out.println("To finish entering lines, press Enter.");
+		input = "0"; // init the var
+				
+		while (!input.equals("")) {
 			input = kbdinput.nextLine();
-			
-			System.out.println("You typed: "+input+". Check LOTR.txt");
-			
-			PrintWriter fileout = new PrintWriter("LOTR.txt");
-			fileout.println(input);
-			fileout.close();
+			if(!input.equals("")){
+				//System.out.println("You typed: "+input+".");
+				fileout.println(input);
+			} else {
+				fileout.println("");
+				fileout.close();
+				System.out.println("Check LOTR.txt");	
+				System.out.println("Program ended");	
+			}
+		}	
 	}
 }
