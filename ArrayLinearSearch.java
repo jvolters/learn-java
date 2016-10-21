a82e8ba8e2c7f069d7775e900571c45380406985
randyvolters <jvolters@live.com>
2016-10-21 12:51:21 +0100

    Added Lesson #49. Changes to be committed: 	new file:   ArrayLinearSearch.class 	new file:   ArrayLinearSearch.java -  simple array search for match 	new file:   IntEqualsTest.class 	new file:   IntEqualsTest.java - simple test of == and !=

---
+++ ArrayLinearSearch.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ArrayLinearSearch {
+	public static void main( String[] args ) {
+		Scanner keyboard = new Scanner(System.in);
+		int[] orderNumbers = { 12345, 54321, 101010, 8675309, 31415, 271828 };
+		int toFind;
+
+		System.out.print("There are " + orderNumbers.length);
+		System.out.println(" orders in the database.");
+
+		System.out.print("Orders: ");
+		// for each integer 'num' in the array 'orderNumbers'...
+		for ( int num : orderNumbers ) {
+			System.out.print( num + " " );
+		}
+		System.out.println();
+
+		System.out.print("Which order to find? ");
+		toFind = keyboard.nextInt();
+
+		for ( int num : orderNumbers ) {
+			if ( num == toFind ) {
+				System.out.println( num + " found.");
+		/* in both for loops the int variable num is declared.
+		   the reason num cannot be declared along with toFind
+		   above where the array is also defined is because of 
+		   for loop syntax for arrays - the variable is local in 
+		   scope to the loop and must be declared there.
+		   so each instance of 'num' is local to the for(each)
+		   loop that uses it.
+		*/
+			}
+		}
+	}
+}
---
+++ IntEqualsTest.java
@@ -0,0 +1,13 @@
+public class IntEqualsTest {
+	public static void main( String[] args ) {
+		int toFind=1, num=20;
+		
+		System.out.println("");
+		if ( toFind == num ){
+			System.out.println( "They are equal!" );
+		}
+		if ( toFind != num ){
+			System.out.println( "They are NOT equal!" );
+		}
+	}
+}	
