a0f2aefd4ba5b28b87ec278a93c405204ca16b0b
randyvolters <jvolters@live.com>
2016-10-26 12:34:13 +0100

    Added notes on what to do to get it working. Changes to be committed: modified:   OutputInput.java

--- OutputInput.java
+++ OutputInput.java
@@ -1,5 +1,15 @@
 /*
 Read file line and write to screen.
+Work in progress...
+For further study:
+http://www.programcreek.com/2011/03/java-read-a-file-line-by-line-code-example/
+https://www.mkyong.com/java8/java-8-stream-read-a-file-line-by-line/
+http://www.avajava.com/tutorials/lessons/how-do-i-read-a-string-from-a-file-line-by-line.html
+
+Also; once I get the reader working, make a change to the program so that the program
+asks the user what file to read.  Allow the user to enter the file (path/filename).
+
+Then read that file.
 */
 import java.io.File;
 import java.util.Scanner;
