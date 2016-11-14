76647bed9d825a2b924ac6d4e63095ae35df7668
randyvolters <jvolters@live.com>
2016-11-14 18:52:52 +0000

    updated with study drills.

--- RectangleDriver.java
+++ RectangleDriver.java
@@ -8,5 +8,13 @@ public class RectangleDriver{
 		/* ----- below is the new way ----- */
 		Rectangle r =  new Rectangle( 10, 5 ); // works because of the constructor
 		System.out.println("The area is: "+ r.getArea() );
+		
+		// study drill below; quck and dirty addition of 2 more rectangles.
+		Rectangle r2 =  new Rectangle( 15, 5 ); // works because of the constructor
+		System.out.println("The area is: "+ r2.getArea() );
+	
+	    Rectangle d2 =  new Rectangle( 20, 10 ); // works because of the constructor
+		System.out.println("The area is: "+ d2.getArea() );
 	}
+	
 }
